957affc52d787f13c57b6d64681b0c5f04a78eda
Andrey <82231196+Badebum@users.noreply.github.com>
2022-05-15 21:13:07 +0300

    hm_4

---
+++ main.js
@@ -0,0 +1,68 @@
+
+// #1
+
+// const squareNum = (number) => number**2
+
+// console.log(squareNum(5));
+
+// #2
+
+// const amountNumber = (a, b) => a + b;
+
+// console.log(amountNumber(5 , 1));
+
+// #3
+
+// const calculations = (num1, num2, num3) => (num1 - num2) / num3;
+
+// console.log(calculations(33,5,2));
+
+// #4
+
+// const weekDays = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+// const dayOfWeek = (day, arrWeek) => arrWeek[day - 1];
+
+// console.log(dayOfWeek(5, weekDays));
+
+// #5
+
+// const numEqual = (value1, value2) => value1 === value2;
+
+// console.log(numEqual(2,2));
+
+// #6
+
+// const numEqual = (value1, value2) => (value1 + value2) > 10;
+
+// console.log(numEqual(20,20));
+
+// #7
+
+// const valueNum = (num) => num > 0 ? true : false;
+
+// console.log(valueNum(5));
+// console.log(valueNum(-5));
+
+// #8
+
+// const isNumberInRange = (num) => num > 0 && num < 10;
+
+// console.log(isNumberInRange(8));
+
+// #9
+
+// const rgb = (r = 0, g = 0, b = 0) => `color: rgb(${r},${g},${b});`;
+
+// console.log(rgb(255, 46, 15));
+
+// #10
+
+// const squareNum = (num) => num ** 2;
+
+// const plusOne = (num) => num+1;
+
+// const calculationsAndCallback = (number, funcSquare, funcPlus) =>
+//     number % 2 === 0 ? funcSquare(number) : funcPlus(number);
+
+// console.log(calculationsAndCallback(7, squareNum, plusOne));
